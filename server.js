ae39edf214f0ffbad17981a6019653288fe618ca
Damon <damonspriggle@hotmail.com>
2022-06-05 21:15:30 -0500

    work in progress honestly

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
-// Initialize sessions
 const sess = {
     secret: process.env.DB_SESSION_SECRET,
     cookie: { maxAge: 7200000 },
@@ -22,26 +21,23 @@ const sess = {
     })
   };
 
-// Initialize the server
+
 const app = express();
-// Define the port for the server
+
 const PORT = process.env.PORT || 3001;
 
-// Give the server a path to the public directory for static files
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set handlebars as the template engine for the server
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// Have Express parse JSON and string data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Tell the app to use Express Session for the session handling
 app.use(session(sess));
 
-// Give the server the path to the routes
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
